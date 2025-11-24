ec6987eee75e13e1eeaeb15a19d8e186236e4161
Samhithpola2025 <91000525+Githubuser1122bruh@users.noreply.github.com>
2025-11-23 23:37:52 -0800

    Fixed particle damping logic and added dynamic mass calculations mde the acceleration handling scale by mass and added all the basic stuff and got UI workingwq

---
+++ build/CMakeFiles/BouncyLabs.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/glad/src/glad.c" "CMakeFiles/BouncyLabs.dir/dependencies/glad/src/glad.c.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/glad/src/glad.c.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/imgui.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/imgui_demo.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_demo.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/imgui_draw.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_draw.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/imgui_tables.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_tables.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/dependencies/imgui/imgui_widgets.cpp" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_widgets.cpp.o.d"
+  "/Users/samhithpola/Developer/GitHub/BouncyLabs/src/main.cpp" "CMakeFiles/BouncyLabs.dir/src/main.cpp.o" "gcc" "CMakeFiles/BouncyLabs.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BouncyLabs.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "BouncyLabs"
+  "BouncyLabs.pdb"
+  "CMakeFiles/BouncyLabs.dir/dependencies/glad/src/glad.c.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/glad/src/glad.c.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/dependencies/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/BouncyLabs.dir/src/main.cpp.o"
+  "CMakeFiles/BouncyLabs.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/BouncyLabs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
